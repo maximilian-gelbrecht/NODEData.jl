d0bcb3b23f02cd086d23f9efcc2f1cb3b33d2cd8
Max <maximilian.gelbrecht@posteo.de>
2022-10-27 21:57:02 +0200

    bug fix valid_set array constructor

--- src/data.jl
+++ src/data.jl
@@ -56,12 +56,13 @@ end
 
 function NODEDataloader(data::AbstractArray{T,N}, t::AbstractArray{U,1}, N_length::Integer; valid_set=nothing) where {T,U,N} 
     @assert size(data)[end] == length(t) "Length of data and t should be equal"
+    @assert 0 <= valid_set < 1 "Valid_set should be ∈ [0,1]"
 
     if isnothing(valid_set)
         return NODEDataloader(DeviceArray(data), t, length(t) - N_length, N_length)
     else 
         N_t = length(t)
-        N_train = Int(floor(0.8 * N_t))
+        N_train = Int(floor((1-valid_set) * N_t))
         N_valid = N_t - N_train 
 
         return NODEDataloader(DeviceArray(data[..,1:N_train]), t[1:N_train], N_train - N_length, N_length), NODEDataloader(DeviceArray(data[..,N_train+1:end]), t[N_train+1:end], N_valid - N_length, N_length)
